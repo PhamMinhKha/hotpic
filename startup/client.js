10e37f43bb9a6ddfba01112a3aafbd8655693f83
pham kha <phamkha@phams-iMac.local>
2021-05-22 11:05:53 +0700

    try rss

--- startup/client.js
+++ startup/client.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { onPageLoad } from 'meteor/server-render';
 import { renderToString } from 'react-dom/server'
+//i dont know how this work
 onPageLoad(async sink => {
   const App = (await import('../imports/ui/App.jsx')).default;
   ReactDOM.hydrate(
